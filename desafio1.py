1cd9105024e9a5739e7ffa17c019a2e31aea9a98
susanagandra <122367270+susanagandra@users.noreply.github.com>
2024-01-11 16:20:41 +0000

    first commit

---
+++ desafio1.py
@@ -0,0 +1,20 @@
+#Desenvolva um programa que peça ao usuário para inserir um número. O programa deve então verificar se 
+# o número é par ou ímpar e exibir uma mensagem informando o usuário.
+
+
+# inserir o numero. A função input obtem um valor inserido. 
+# int() converte o valor num numero inteiro.
+
+
+num = int(input("Por favor, insira um número: "))
+
+
+# verifica se o numero é par ou impar
+
+if num == 0:
+    print(f"O número {num} é par.")
+elif num % 2 == 0:
+    print(f"O número {num} é par.")
+else:
+    print(f"O número {num} é ímpar.")
+
