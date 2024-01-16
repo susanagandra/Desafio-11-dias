341bfd0e81c2ddd19e382adae8421ac4a1a6892d
susanagandra <122367270+susanagandra@users.noreply.github.com>
2024-01-16 10:33:52 +0000

    refractor desafio5

--- desafio5.py
+++ desafio5.py
@@ -3,24 +3,15 @@ import random
 # Desenvolva um script que gere e exiba uma lista com 6 números aleatórios entre 1 e 60, sem repetições (ou seja, dentre os seis números selecionados, 
 # não podem existir números repetidos), para que você possa fazer um jogo da Mega Sena.
 
+mega_numbers = []
 
-def num_mega():
-    random_num = []
+while len(mega_numbers) < 6:
 
-    while len(random_num) < 6:
-        num = random.randint(1, 60)
+    num = random.randint(1, 60) #gera um numero aleatório 1-60
 
-        # Verifica se o número já foi gerado
-        if num not in random_num:
-            random_num.append(num)
+    if num not in mega_numbers:     #verifica se o numero está na lista
+        mega_numbers.append(num)    #adiciona se não estiver presente
 
-    return random_num
+print ("os seus números são:", mega_numbers)
 
-def show_num_mega(nums):
-    print("Os seus números:")
-    for num in nums:
-        print(num)
-
-num_mega_created = num_mega()
-show_num_mega(num_mega_created)
 
