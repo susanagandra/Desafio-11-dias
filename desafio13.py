f4a700a6c8c7efd5b3f0fa82f603731e91cdfdd1
susanagandra <122367270+susanagandra@users.noreply.github.com>
2024-01-24 11:16:31 +0000

    refractor desafio 13

--- desafio13.py
+++ desafio13.py
@@ -6,10 +6,10 @@ def check_valid_email (email):
     pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     return bool(re.match(pattern, email))
 
-user_email = input("Please enter your email: ")
+email = input("Please enter your email: ")
 
-if check_valid_email(user_email):
-    print(f"{user_email}  Valid email.")
+if check_valid_email(email):
+    print(f"{email}  Valid email.")
 else:
-    print(f"{user_email} Invalid email.")
+    print(f"{email} Invalid email.")
   
